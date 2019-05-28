5b928caf357136226e3090140e01db1f7cc7b8da
Tahoooe <50768582+Tahoooe@users.noreply.github.com>
2019-05-28 15:22:42 +0200

    Add files via upload

---
+++ src/scripts/lang.js
@@ -0,0 +1,110 @@
+var dict = {
+  "Settings": {
+    fr: "Réglages"
+  },
+  "Background": {
+    fr: "Arrière plan"
+  },
+  "Choose background": {
+    fr: "Choisir l'arrière plan"
+  }
+  "Custom background": {
+    fr: "Arrière plan personnalisé"
+  }
+  "Blur intensity": {
+    fr: "Intensité du flou"
+  }
+  "Quick links": {
+    fr: "Liens favoris"
+  }
+  "Title": {
+    fr: "Titre"
+  }
+  "Add": {
+    fr: "Ajouter"
+  }
+  "Weather": {
+    fr: "Météo"
+  }
+  "Change city": {
+    fr: "Changer la ville"
+  }
+  "Imperial units": {
+    fr: "Unités impériales"
+  }
+  "Good morning": {
+    fr: "Bonne matinée"
+  }
+  "Good Afternoon": {
+    fr: "Bonne après midi"
+  }
+  "Good evening": {
+    fr: "Bonne soirée"
+  }
+  "Good night": {
+    fr: "Bonne nuit"
+  }
+  "Monday": {
+    fr: "Lundi"
+  }
+  "Tuesday": {
+    fr: "Mardi"
+  }
+  "Wednesday": {
+    fr: "Mercredi"
+  }
+  "Thursday": {
+    fr: "Jeudi"
+  }
+  "Friday": {
+    fr: "Vendredi"
+  }
+  "Saturday": {
+    fr: "Samedi"
+  }
+  "Sunday": {
+    fr: "Dimanche"
+  }
+  "January": {
+    fr: "Janvier"
+  }
+  "February": {
+    fr: "Février"
+  }
+  "March": {
+    fr: "Mars"
+  }
+  "April": {
+    fr: "Avril"
+  }
+  "May": {
+    fr: "Mai"
+  }
+  "June": {
+    fr: "Juin"
+  }
+  "July": {
+    fr: "Juillet"
+  }
+  "August": {
+    fr: "Août"
+  }
+  "September": {
+    fr: "Septembre"
+  }
+  "October": {
+    fr: "Octobre"
+  }
+  "November": {
+    fr: "Novembre"
+  }
+  "December": {
+    fr: "Décembre"
+  }
+  "Language": {
+    fr: "Langue"
+  }
+  "Choose language": {
+    fr: "Choix de la langue"
+  }
+}
