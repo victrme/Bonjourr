b0cc7887b28d3bcb17eb9a24ca7f4c68d3420e94
Tahoooe <50768582+Tahoooe@users.noreply.github.com>
2019-05-28 13:57:58 +0200

    Ajout de la signature aléatoire

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -509,6 +509,13 @@ $(".imgpreview img").click(function() {
 
 
 
+// Signature aléatoire
+$(document).ready(function() {
+    var quotes = new Array("<p>Made in France with ❤️<br> by <a href='https://victor-azevedo.me/'>Victor Azevedo</a> & <a href='https://tahoe.be'>Tahoe Beetschen</a></p>", "<p>Made in France with ❤️<br> by <a href='https://tahoe.be'>Tahoe Beetschen</a> & <a href='https://victor-azevedo.me/'>Victor Azevedo</a></p>");
+    var randno = Math.floor(Math.random()*(quotes.length));
+    $('.signature').append(quotes[randno]);
+    console.log(randno);
+});
 
 
 
