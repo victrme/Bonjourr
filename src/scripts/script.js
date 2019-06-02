0928c5062e013e284d1bc1f6a7951e600c2281aa
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2019-06-02 21:00:16 +0200

    Revert "Appui pour supprimer"
    This reverts commit b5863b7d2ecaeff95a86d1f5c3edd395ec1a9036.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -129,59 +129,29 @@ function initblocks() {
 //affiche le bouton pour suppr le link
 function showRemoveLink() {
 
-	//envoie l'url du a dans un href temporaire
-	//ou inversement
-	function hrefHandling(state) {
-		$(".block a").each(function() {
-			
-			if (state === "disable") {
-				var h = $(this).attr("href");
-				$(this).attr("hreftemp", h);
-				$(this).removeAttr("href");
-			}
-			if (state === "enable") {
-				var h = $(this).attr("hreftemp");
-				$(this).attr("href", h);
-				$(this).removeAttr("hreftemp");
-			}
-		});
-	}
 
 	var remTimeout;
 	var canRemove = false;
 
-
 	//utilise on pour le dom rajouté après le document.load
-	//si la souris appuie sur un block
-	$(".linkblocks").on("mousedown", ".block", function(e) {
+	$(".linkblocks").on("mouseenter", ".block", function(e) {
 
-		//enleve le href, rejoute les classes
-		//focus linkblocks et active le remove
 		remTimeout = setTimeout(function() {
 
-			hrefHandling("disable");
-
-			$(".block").find(".remove").addClass("visible");
-			$(".block").addClass("wiggly");
-			$(".linkblocks").focus();
+			var block = e.currentTarget;
+			$(block).find(".remove").addClass("visible");
 
 			canRemove = true;
 
 		}, 1000);
 	});
 
-	//si linkblocks perd le focus
-	$(".linkblocks").on("focusout", ".block", function(e) {
+	$(".linkblocks").on("mouseleave", ".block", function(e) {
 
-		//enleve le timeout si <1000ms
-		//rajoute le href, enleve les classes
-		//désactive le remove
 		clearTimeout(remTimeout);
 
-		hrefHandling("enable");
-
-		$(".block").find(".remove").removeClass("visible");
-		$(".block").removeClass("wiggly");
+		var block = e.currentTarget;
+		$(block).find(".remove").removeClass("visible");
 
 		canRemove = false;
 	});
