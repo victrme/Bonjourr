ce2ab00003d2cd5e66947c88d3ac8fa401e0342b
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2019-10-03 17:36:49 +0200

    Revert "1.7.1"
    This reverts commit 626a6dfba2c81265620471c07599bdec36b13f15.

--- Github/src/scripts/background.js
+++
@@ -1,8 +0,0 @@
-
-chrome.storage.sync.get("lang", (lang) => {
-
-	let url = "https://bonjourr.fr/" + (lang === "fr" ? "fr/" : "");
-	url += (navigator.userAgent.includes("Chrome") ? "goodbye_chrome" : "goodbye_firefox");
-
-	chrome.runtime.setUninstallURL(url);
-});
