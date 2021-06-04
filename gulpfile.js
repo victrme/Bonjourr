cd44d5a89e153f76327d1edd552e512b05394188
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2021-06-04 08:08:42 +0200

    Chrome & Firefox gulp bundling

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,71 @@
-const {series, parallel, src, dest, watch, pipe} = require('gulp');
-const concat = require('gulp-concat');
-const minify = require('gulp-babel-minify');
-const htmlmin = require('gulp-htmlmin');
-const csso = require('gulp-csso');
+const { series, parallel, src, dest, watch } = require('gulp')
+const concat = require('gulp-concat')
+const minify = require('gulp-babel-minify')
+const htmlmin = require('gulp-htmlmin')
+const csso = require('gulp-csso')
+const rename = require('gulp-rename')
 
 const path = {
-    scss: ["src/styles/scss/_global.scss", "src/styles/scss/_media.scss", "src/styles/scss/_mixins.scss", "src/styles/scss/style.scss"],
-    css: ["src/styles/style.css", "src/styles/events.css"],
-    js: ["src/scripts/lang.js", "src/scripts/script.js", "src/scripts/settings.js"]
+	scss: [
+		'src/styles/scss/_global.scss',
+		'src/styles/scss/_media.scss',
+		'src/styles/scss/_mixins.scss',
+		'src/styles/scss/style.scss',
+	],
+	css: ['src/styles/style.css', 'src/styles/events.css'],
+	js: ['src/scripts/lang.js', 'src/scripts/script.js', 'src/scripts/settings.js'],
 }
 
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+function scssTask() {
+	return src(path.scss)
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(sourcemaps.write('.'))
+		.pipe(concat('style.css'))
+		.pipe(dest('src/styles'))
 }
 
-function scssTask(){
-    return src(path.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('.'))
-        .pipe(concat('style.css'))
-        .pipe(dest("src/styles")
-    );
-}
-
-function cssTask(){
-    return src(path.css)
-        .pipe(csso())
-        .pipe(concat('style.css'))
-        .pipe(dest("release/src/styles")
-    );
+function cssTask() {
+	return src(path.css).pipe(csso()).pipe(concat('style.css')).pipe(dest('release/src/styles'))
 }
 
 function htmlTask() {
-    return src(["index.html", "settings.html"])
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true
-    }))
-    .pipe(dest('release/'));
+	return src(['index.html', 'settings.html'])
+		.pipe(
+			htmlmin({
+				collapseWhitespace: true,
+				removeComments: true,
+			})
+		)
+		.pipe(dest('release/'))
 }
 
 function jsTask() {
-    return src(path.js)
-        .pipe(concat('main.js'))
-        .pipe(minify({
-            mangle: {
-                    keepClassName: true
-                }
-            }))
-        .pipe(dest('release/src/scripts')
-    );
+	return src(path.js)
+		.pipe(concat('main.js'))
+		.pipe(
+			minify({
+				mangle: {
+					keepClassName: true,
+				},
+			})
+		)
+		.pipe(dest('release/src/scripts'))
 }
 
-
-function watchTask(){
-    watch(
-        path.css,
-        parallel(scssTask)
-    );
+function manifestTask(which) {
+	return src(`manifest-${which}.json`).pipe(rename('manifest.json')).pipe(dest('release/'))
 }
 
-exports.default = series(
-    parallel(cssTask, jsTask, htmlTask)/*,
-    watchTask*/);
+function watchTask() {
+	watch(
+		path.css,
+		parallel(scssTask, jsTask, htmlTask, () => manifestTask('chrome'))
+	)
+}
+
+exports.chrome = series(parallel(cssTask, jsTask, htmlTask, () => manifestTask('chrome')))
+exports.firefox = series(parallel(cssTask, jsTask, htmlTask, () => manifestTask('firefox')))
+exports.online = series(parallel(cssTask, jsTask, htmlTask))
+
+exports.watcher = watchTask
