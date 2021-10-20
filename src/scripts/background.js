463b08bbf01122844b27d80a7ad0c992b3276fae
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2021-10-20 10:36:29 +0200

    Updated chrome to manifest v3

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,19 +1,11 @@
-function newTab() {
-	chrome.tabs.create({
-		url: 'chrome://newtab',
-	})
-}
-
-function goodbye() {
-	return 'https://bonjourr.fr/goodbye' + (isOnChrome ? '?from=Chrome' : '?from=Firefox')
-}
-
+const newTab = () => chrome.tabs.create({ url: 'chrome://newtab' })
+const goodbye = () => 'https://bonjourr.fr/goodbye' + (isOnChrome ? '?from=Chrome' : '?from=Firefox')
 const isOnChrome = navigator.userAgent.includes('Chrome')
 
 chrome.runtime.setUninstallURL(goodbye())
 
 if (isOnChrome) {
-	chrome.browserAction.onClicked.addListener(newTab)
+	chrome.action.onClicked.addListener(newTab)
 	chrome.runtime.onInstalled.addListener(function (d) {
 		if (d && d.reason && d.reason == 'install') newTab()
 	})
