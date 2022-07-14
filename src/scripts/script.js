ce691d568d3a748c064274d3dc132c379f51031a
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-07-14 20:53:16 +0200

    Removed rearrange mess ( for now )

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -231,24 +231,7 @@ function clock(event, init) {
 }
 
 function quickLinks(event, that, init) {
-	// Pour ne faire qu'un seul storage call
-	// [{ index: number, url: string }]
-
 	const domlinkblocks = id('linkblocks')
-  let draggedId = ''
-	let pivots = []
-	let coords = []
-	let movedCoords = []
-	let startsDrag = false
-	let elemsPerRow = 0
-	let startMousePosition = { x: 0, y: 0 }
-	let posDiffX, posDiffY, width, height
-
-	const newIndex = (id) => movedCoords.findIndex((a) => a._id === id)
-
-	Array.prototype.move = function (from, to) {
-		this.splice(to, 0, this.splice(from, 1)[0]) // https://stackoverflow.com/a/7180095
-	}
 
 	async function initblocks(links, linksrow) {
 		//
@@ -327,7 +310,6 @@ function quickLinks(event, that, init) {
 			}
 
 			try {
-
 				// Add blocks and events
 				const blocklist = links.map((l) => createBlock(l))
 				blocklist.forEach(({ block }) => addEvents(block))
@@ -404,127 +386,6 @@ function quickLinks(event, that, init) {
 				elem.addEventListener('touchstart', startHandler, { passive: true })
 				elem.addEventListener('touchend', endHandler, { passive: true })
 			})()
-
-		// Drags
-		elem.onmousedown = function (e) {
-			e.stopPropagation()
-
-			// Initialise toute les coordonnees
-			// Defini l'ID de l'element qui se deplace
-			// Defini la position de la souris pour pouvoir offset le deplacement de l'elem
-			const { x, y } = e
-
-			draggedId = e.composedPath().find((e) => e.className === 'block_parent').id
-			elemsPerRow = document.querySelectorAll('#linkblocks_inner > div')[0].childElementCount
-			startMousePosition = { x, y }
-			startsDrag = true
-
-			// Est deja sort puisque c'est par ordre d'apparition dans le dom
-			document.querySelectorAll('.block_parent').forEach((block) => {
-				const { x, y } = block.getBoundingClientRect()
-				coords.push({ _id: block.id, x, y })
-			})
-
-			let rowsPos = [] // Pour calculer la hauteur des lignes
-			coords.forEach(({ y }) => {
-				if (y !== rowsPos[rowsPos.length - 1]) rowsPos.push(y)
-			})
-
-			height = rowsPos.length > 1 ? rowsPos[1] - rowsPos[0] : rowsPos[0]
-			width = coords[1].x - coords[0].x
-			movedCoords = [...coords]
-
-			// Determine la position les pivots (ceux qui change de ligne)
-			coords.forEach((c, i) => {
-				if (i % elemsPerRow === 0) pivots.push(i)
-			})
-
-			id(draggedId).style.zIndex = '4'
-			id(draggedId).style.cursor = 'grabbing'
-			id(draggedId).style.transition = 'none'
-		}
-	}
-
-	function linksDragging() {
-		let [curr, prev, currrow, prevrow, firstPos, ox, oy] = [0, 0, 0, 0, 0, 0, 0]
-
-		const deplaceElem = (dom, x, y) => {
-			id(dom).style.transform = `translateX(${x}px) translateY(${y}px)`
-		}
-
-		domlinkblocks.onmousemove = function (e) {
-			if (startsDrag) {
-				posDiffX = e.x - startMousePosition.x
-				posDiffY = e.y - startMousePosition.y
-				currrow = 0
-
-				curr = coords.findIndex(({ x }) => e.x <= x + width) // x est la position de l'element, e.x celle de la souris. gauche a droite
-				coords.forEach(({ y }) => (y + height < e.y ? currrow++ : '')) // meme chose pour les lignes, si la hauteur depasse, la ligne change
-
-				curr += currrow // ajoute la quantite d'elements des lignes au dessus de la souris
-
-				// trigger si les indexes sont differents
-				if (currrow !== prevrow || (prev !== curr && curr >= 0)) {
-					movedCoords.move(newIndex(draggedId), curr)
-
-					movingAmount = [] // Compare la position de chaque id entre leur pos initiale et a chaque changements
-
-					movedCoords.forEach((elem, i) => {
-						firstPos = coords.findIndex((a) => a._id === elem._id)
-						ox = i - firstPos // calcule la diff
-						oy = 0
-
-						// ox is used as direction here (because only 1 or -1)
-						if (ox !== 0 && pivots.includes(ox < 0 ? firstPos : i)) {
-							oy = ox // +/- une ligne
-							ox = -ox * (elemsPerRow - 1) // parcours la longeur d'une ligne
-						}
-
-						deplaceElem(elem._id, ox * width, oy * height) // se deplace de la diff * la taille d'un elem
-						movingAmount.push([ox, oy])
-					})
-
-					// console.clear()
-					// console.table(movingAmount)
-
-					// les deux indexes deviennent similaires
-					prev = curr
-					prevrow = currrow
-				}
-
-				id(draggedId).style.transform = `translateX(${posDiffX}px) translateY(${posDiffY}px)`
-			}
-		}
-
-		const endDrags = () => {
-			if (draggedId && startsDrag) {
-				const i = newIndex(draggedId)
-				const x = coords[i].x - movedCoords[i].x
-				const y = coords[i].y - movedCoords[i].y
-
-				id(draggedId).setAttribute('style', `cursor: pointer; transform: translate(${x}px, ${y}px)`)
-
-				setTimeout(() => {
-					chrome.storage.sync.get(null, (data) => {
-						document.querySelectorAll('.block_parent').forEach((block) => block.remove())
-						movedCoords.forEach((elem, i) => (data[elem._id].order = i))
-
-						slowRange({ ...data })
-						initblocks(bundleLinks(data), data.linksrow)
-					})
-				}, 300)
-
-				startsDrag = false
-				prev = curr = 0
-				pivots = []
-				coords = []
-				movingAmount = []
-			}
-		}
-
-		// end drag
-		domlinkblocks.onmouseup = endDrags
-		domlinkblocks.onmouseout = endDrags
 	}
 
 	function editEvents() {
