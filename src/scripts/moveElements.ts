9a52070ee485f61088c6079b67fdf2c8a52fa777
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-11-07 10:18:39 +0100

    Reduced update mutations, working responsive

--- src/scripts/moveElements.ts
+++ src/scripts/moveElements.ts
@@ -1,15 +1,17 @@
 import clamp from 'lodash.clamp'
 import clonedeep from 'lodash.clonedeep'
 import storage from './storage'
-import { Move, MoveKeys, MoveItem } from './types/sync'
+import { Move, MoveKeys, MoveItem, Sync } from './types/sync'
 import { syncDefaults, clas } from './utils'
 
-export default function moveElements(move: Move) {
+export default function moveElements(init: Move) {
 	const doms = '#time, #main, #sb_container, #notes_container, #linkblocks, #quotes_container'
 	const dominterface = document.querySelector<HTMLElement>('#interface')
 	const selectables = document.querySelectorAll<HTMLElement>(doms)
 	let selectedDOM: HTMLElement | null
 	let selectedID: MoveKeys | null
+	let smallWidth = false
+	let lastSelection: Move['selection']
 
 	type Layout = Move['layouts'][keyof Move['layouts']]
 
@@ -80,26 +82,6 @@ export default function moveElements(move: Move) {
 		e ? (e.key === 'm' ? toggle() : '') : toggle()
 	}
 
-	function toggleElementSelection(elem: HTMLElement) {
-		const layout = move.layouts[move.selection]
-		const id = elem?.dataset.moveId || elem?.id
-
-		removeSelection()
-
-		// Only remove selection modifiers if failed to get id
-		if (!(id in layout.items)) {
-			return
-		}
-
-		btnSelectionAlign(layout.items[id as MoveKeys])
-		gridMoveEdgesControl(layout.grid, id as MoveKeys)
-
-		elem.classList.add('move-selected') // add clicked
-
-		selectedDOM = elem as HTMLElement
-		selectedID = id as MoveKeys
-	}
-
 	function gridMoveEdgesControl(grid: Layout['grid'], id: MoveKeys) {
 		const { row, col } = getItemPosition(grid, id)
 
@@ -128,9 +110,10 @@ export default function moveElements(move: Move) {
 		textBtns.forEach((b) => clas(b, b.dataset.align === item?.text, 'selected'))
 	}
 
-	function resetBtnControl(layout: Layout) {
+	function resetBtnControl(move: Move) {
 		const btn = document.querySelector<HTMLButtonElement>('#reset-layout')
 		const defaultGrid = syncDefaults.move.layouts[move.selection].grid
+		const layout = move.layouts[move.selection]
 
 		const isSameGrid = layoutToGridAreas(layout.grid) === layoutToGridAreas(defaultGrid)
 		const isSameAlign = Object.values(layout.items).filter(({ box, text }) => box !== '' || text !== '').length === 0
@@ -146,92 +129,165 @@ export default function moveElements(move: Move) {
 	// Updates
 	//
 
-	function gridChange(button: HTMLButtonElement) {
-		if (!selectedDOM || !selectedID) return
-
-		const { grid } = move.layouts[move.selection]
-
-		// Get button move amount
-		const y = parseInt(button.dataset.row || '0')
-		const x = parseInt(button.dataset.col || '0')
-
-		// Get current row / col
-		const currR = grid.findIndex((row) => row.find((col) => col === selectedID))
-		const currC = grid[currR].findIndex((col) => col === selectedID)
-
-		// Update row / col
-		const newR = clamp(currR + y, 0, grid.length - 1)
-		const newC = clamp(currC + x, 0, grid[0].length - 1)
-
-		// swap items
-		let tempItem = grid[currR][currC]
-		grid[currR][currC] = grid[newR][newC]
-		grid[newR][newC] = tempItem
-
-		// Apply changes
-		setGridAreas(grid)
-
-		move.layouts[move.selection].grid = grid
-		storage.sync.set({ move: move })
-
-		gridMoveEdgesControl(grid, selectedID)
-		resetBtnControl(move.layouts[move.selection])
+	type Update = {
+		is: 'select' | 'grid' | 'box' | 'text' | 'layout' | 'reset' | 'responsive'
+		button?: HTMLButtonElement
+		elem?: HTMLElement
 	}
 
-	function alignChange(button: HTMLButtonElement, type: 'box' | 'text') {
-		if (!selectedDOM || !selectedID) return
+	function updateMoveElement({ is, elem, button }: Update) {
+		storage.sync.get('move', (data) => {
+			let move: Move
 
-		const layout = move.layouts[move.selection]
-		const item = layout.items[selectedID]
+			// Check if storage has move, if not, use (/ deep clone) default move
+			move = 'move' in data ? data.move : clonedeep(syncDefaults.move)
 
-		item[type] = button.dataset.align || ''
+			// force single on small width
+			if (smallWidth) {
+				move.selection = 'single'
+			}
 
-		setAlign(selectedDOM, item)
-		btnSelectionAlign(item)
-		resetBtnControl(layout)
+			function gridChange() {
+				if (!selectedDOM || !selectedID || !button) return
 
-		// Update storage
-		move.layouts[move.selection].items[selectedID] = item
-		storage.sync.set({ move: move })
-	}
+				const { grid } = move.layouts[move.selection]
 
-	function layoutChange(button: HTMLButtonElement) {
-		if (!((button.dataset.layout || 'triple') in move.layouts)) return // button dataset is wrong somehow
+				// Get button move amount
+				const y = parseInt(button.dataset.row || '0')
+				const x = parseInt(button.dataset.col || '0')
 
-		// Update selection
-		move.selection = (button.dataset.layout || 'triple') as Move['selection']
+				// Get current row / col
+				const currR = grid.findIndex((row) => row.find((col) => col === selectedID))
+				const currC = grid[currR].findIndex((col) => col === selectedID)
 
-		// Use new selection
-		const layout = move.layouts[move.selection]
+				// Update row / col
+				const newR = clamp(currR + y, 0, grid.length - 1)
+				const newC = clamp(currC + x, 0, grid[0].length - 1)
 
-		setAllAligns(layout.items)
-		setGridAreas(layout.grid)
-		resetBtnControl(layout)
-		btnSelectionLayout(move.selection)
+				// swap items
+				let tempItem = grid[currR][currC]
+				grid[currR][currC] = grid[newR][newC]
+				grid[newR][newC] = tempItem
 
-		if (selectedID) {
-			gridMoveEdgesControl(layout.grid, selectedID)
-			btnSelectionAlign(layout.items[selectedID])
-		}
+				// Apply changes
+				setGridAreas(grid)
+				move.layouts[move.selection].grid = grid
 
-		storage.sync.set({ move: move })
-	}
+				gridMoveEdgesControl(grid, selectedID)
+				resetBtnControl(move)
 
-	function layoutReset() {
-		// Todo: don't select layout manually
-		const { single, double, triple } = syncDefaults.move.layouts
-		if (move.selection === 'single') move.layouts.single = clonedeep(single)
-		if (move.selection === 'double') move.layouts.double = clonedeep(double)
-		if (move.selection === 'triple') move.layouts.triple = clonedeep(triple)
+				storage.sync.set({ move: move })
+			}
 
-		const layout = move.layouts[move.selection]
+			function alignChange(type: 'box' | 'text') {
+				if (!selectedDOM || !selectedID || !button) return
 
-		setAllAligns(layout.items)
-		setGridAreas(layout.grid)
-		resetBtnControl(layout)
-		removeSelection()
+				const layout = move.layouts[move.selection]
+				const item = layout.items[selectedID]
 
-		storage.sync.set({ move: move })
+				item[type] = button.dataset.align || ''
+
+				setAlign(selectedDOM, item)
+				btnSelectionAlign(item)
+				resetBtnControl(move)
+
+				// Update storage
+				move.layouts[move.selection].items[selectedID] = item
+				storage.sync.set({ move: move })
+			}
+
+			function layoutChange(selection?: Move['selection']) {
+				if (!selection && button) {
+					// button dataset is wrong somehow
+					if (!((button.dataset.layout || 'triple') in move.layouts)) return
+				}
+
+				// Only update selection if coming from user
+				if (is !== 'responsive' && button) {
+					move.selection = (button.dataset.layout || 'triple') as Move['selection']
+					storage.sync.set({ move: move })
+				}
+
+				// Use new selection
+				const layout = move.layouts[move.selection]
+
+				setAllAligns(layout.items)
+				setGridAreas(layout.grid)
+				resetBtnControl(move)
+				btnSelectionLayout(move.selection)
+
+				if (selectedID) {
+					gridMoveEdgesControl(layout.grid, selectedID)
+					btnSelectionAlign(layout.items[selectedID])
+				}
+			}
+
+			function layoutReset() {
+				// Todo: don't select layout manually
+				const { single, double, triple } = syncDefaults.move.layouts
+				if (move.selection === 'single') move.layouts.single = clonedeep(single)
+				if (move.selection === 'double') move.layouts.double = clonedeep(double)
+				if (move.selection === 'triple') move.layouts.triple = clonedeep(triple)
+
+				const layout = move.layouts[move.selection]
+
+				setAllAligns(layout.items)
+				setGridAreas(layout.grid)
+				resetBtnControl(move)
+				removeSelection()
+
+				storage.sync.set({ move: move })
+			}
+
+			function elementSelection() {
+				const layout = move.layouts[move.selection]
+				const id = elem?.dataset.moveId || elem?.id
+
+				removeSelection()
+
+				// Only remove selection modifiers if failed to get id
+				if (!id || !(id in layout.items)) {
+					return
+				}
+
+				btnSelectionAlign(layout.items[id as MoveKeys])
+				gridMoveEdgesControl(layout.grid, id as MoveKeys)
+
+				elem.classList.add('move-selected') // add clicked
+
+				selectedDOM = elem as HTMLElement
+				selectedID = id as MoveKeys
+			}
+
+			switch (is) {
+				case 'select':
+					elementSelection()
+					break
+
+				case 'grid':
+					gridChange()
+					break
+
+				case 'box':
+					alignChange('box')
+					break
+
+				case 'text':
+					alignChange('text')
+					break
+
+				case 'layout':
+					layoutChange()
+					break
+
+				case 'reset':
+					layoutReset()
+
+				case 'responsive':
+					layoutChange()
+					break
+			}
+		})
 	}
 
 	//
@@ -239,33 +295,16 @@ export default function moveElements(move: Move) {
 	//
 
 	;(function initilisation() {
-		function changeLayout(isMobile: boolean, init?: boolean) {
-			const layout = isMobile ? move.layouts.single : move.layouts[move.selection]
-
-			mobileWidth = isMobile
-
-			setAllAligns(layout.items)
-			setGridAreas(layout.grid)
-			btnSelectionLayout(isMobile ? 'single' : move.selection)
-
-			// Don't set align / grid buttons on init
-			if (!init && selectedID) {
-				resetBtnControl(layout)
-				gridMoveEdgesControl(layout.grid, selectedID)
-				btnSelectionAlign(layout.items[selectedID])
-			}
+		// Detect small width on startup
+		if (window.innerWidth < 764) {
+			init.selection = 'single'
 		}
 
-		let mobileWidth = false
+		const layout = init.layouts[init.selection]
 
-		// Init layout change
-		changeLayout(window.innerWidth < 764, true)
-
-		// Trigger a layout change when width is crosses threshold
-		window.addEventListener('resize', () => {
-			if (window.innerWidth < 764 && !mobileWidth) changeLayout(true)
-			if (window.innerWidth > 764 && mobileWidth) changeLayout(false)
-		})
+		setAllAligns(layout.items)
+		setGridAreas(layout.grid)
+		btnSelectionLayout(init.selection)
 	})()
 
 	//
@@ -276,26 +315,36 @@ export default function moveElements(move: Move) {
 		document.addEventListener('keypress', toggleMoveStatus)
 
 		selectables.forEach((elem) => {
-			elem.addEventListener('click', () => toggleElementSelection(elem))
+			elem.addEventListener('click', () => updateMoveElement({ is: 'select', elem: elem }))
 		})
 
 		document.querySelectorAll<HTMLButtonElement>('#grid-layout button').forEach((button) => {
-			button.addEventListener('click', () => layoutChange(button))
+			button.addEventListener('click', () => updateMoveElement({ is: 'layout', button }))
 		})
 
 		document.querySelectorAll<HTMLButtonElement>('#grid-mover button').forEach((button) => {
-			button.addEventListener('click', () => gridChange(button))
+			button.addEventListener('click', () => updateMoveElement({ is: 'grid', button }))
 		})
 
 		document.querySelectorAll<HTMLButtonElement>('#box-alignment-mover button').forEach((button) => {
-			button.addEventListener('click', () => alignChange(button, 'box'))
+			button.addEventListener('click', () => updateMoveElement({ is: 'box', button }))
 		})
 
 		document.querySelectorAll<HTMLButtonElement>('#text-alignment-mover button').forEach((button) => {
-			button.addEventListener('click', () => alignChange(button, 'text'))
+			button.addEventListener('click', () => updateMoveElement({ is: 'text', button }))
 		})
 
-		document.querySelector<HTMLButtonElement>('#reset-layout')?.addEventListener('click', layoutReset)
+		document
+			.querySelector<HTMLButtonElement>('#reset-layout')
+			?.addEventListener('click', () => updateMoveElement({ is: 'reset' }))
 		document.querySelector<HTMLButtonElement>('#close-mover')?.addEventListener('click', () => toggleMoveStatus())
+
+		// Trigger a layout change when width is crosses threshold
+		window.addEventListener('resize', () => {
+			if (window.innerWidth < 764 && !smallWidth) smallWidth = true
+			if (window.innerWidth > 764 && smallWidth) smallWidth = false
+
+			updateMoveElement({ is: 'responsive' })
+		})
 	}, 200)
 }
