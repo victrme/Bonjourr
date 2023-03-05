victrme/Bonjourr

commit 0c4eba3ce0e5f9e5c583a3e6db482e65caa8bd2e

Improved startup display using body classes

src/index.html
@@ -16,7 +16,7 @@
 		<link rel="preconnect" href="https://fonts.googleapis.com" />
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 
-		<link rel="icon" href="src/assets/favicon-128x128.png" />
+		<link rel="icon" href="src/assets/favicon.ico" />
 		<link rel="apple-touch-icon" href="src/assets/apple-touch-icon.png" />
 		<!-- manifest -->
 
@@ -25,7 +25,7 @@
 		<style type="text/css" id="fontstyle"></style>
 	</head>
 
-	<body class="autodark">
+	<body class="autodark init loading">
 		<button id="skiptosettings" type="button">Skip to Settings</button>
 
 		<div id="editlink">
@@ -164,7 +164,7 @@
 			</div>
 		</div>
 
-		<main id="interface" class="init">
+		<main id="interface">
 			<div id="time" class="hidden">
 				<div id="time-container" data-hide="clock">
 					<h1 id="clock" class="hidden"></h1>
@@ -231,7 +231,7 @@
 			<div id="credit"></div>
 		</div>
 
-		<div id="showSettings" class="init" data-hide="settingsicon">
+		<div id="showSettings" data-hide="settingsicon">
 			<button aria-label="Toggle settings menu">
 				<img src="src/assets/interface/gear.svg" alt="" draggable="false" />
 			</button>

src/scripts/index.ts
@@ -1394,7 +1394,7 @@ export function darkmode(value: 'auto' | 'system' | 'enable' | 'disable', isEven
 			disable: '',
 		}
 
-		document.body.setAttribute('class', cases[value])
+		clas(document.body, true, cases[value])
 
 		if (isEvent) {
 			storage.sync.set({ dark: value })
@@ -2025,23 +2025,17 @@ export function canDisplayInterface(cat: keyof typeof functionsLoad | null, init
 	//
 	// Progressive anim to max of Bonjourr animation time
 	function displayInterface() {
-		const domshowsettings = $('showSettings') as HTMLDivElement
-		let loadtime = performance.now() - loadtimeStart
+		let loadtime = Math.min(performance.now() - loadtimeStart, 400)
 
-		if (loadtime > 400) loadtime = 400
-		loadtime = loadtime < 33 ? 0 : 400
+		if (loadtime < 33) {
+			loadtime = 0
+		}
 
-		domshowsettings.style.transition = `opacity ${loadtime}ms`
-		dominterface.style.transition = `opacity ${loadtime}ms, transform .4s`
-		dominterface.style.opacity = '1'
-
-		clas(domshowsettings, true, 'enabled')
+		document.documentElement.style.setProperty('--load-time-transition', loadtime + 'ms')
+		document.body.classList.remove('loading')
 
 		setTimeout(() => {
-			dominterface.classList.remove('init')
-			domshowsettings.classList.remove('init')
-			domshowsettings.style.transition = ``
-
+			document.body.classList.remove('init')
 			storage.sync.get(null, (data) => settingsInit(data as Sync))
 		}, loadtime + 100)
 	}

src/styles/_global.scss
@@ -28,6 +28,7 @@ $easeOutThree: cubic-bezier(0.215, 0.61, 0.355, 1);
 :root {
 	--app-height: 100vh;
 	--notes-width: 50em;
+	--load-time-transition: 0ms;
 	--average-color: black;
 	--text-shadow-alpha: 0.2;
 	--notes-background-alpha: 0.1;
@@ -121,6 +122,33 @@ body {
 			outline: 2px solid var(--blue);
 		}
 	}
+
+	&.loading {
+		#interface,
+		#showSettings,
+		#creditContainer,
+		#skiptosettings {
+			opacity: 0;
+		}
+	}
+
+	&.init {
+		#interface,
+		#showSettings,
+		#creditContainer,
+		#skiptosettings {
+			transition: opacity var(--load-time-transition);
+		}
+
+		#showSettings button {
+			transition: background-color 0s;
+		}
+	}
+
+	.he_hidden,
+	#interface #main #weather #tempContainer.he_hidden {
+		display: none;
+	}
 }
 
 @mixin allCentered {
@@ -146,40 +174,6 @@ body {
 	// 	background: rgba($fbTint, $fbTint, $fbTint, $fbAlpha);
 	// 	color: $fbColor;
 	// }
-}
-
-@mixin hideElements {
-	#interface {
-		//
-		&.init {
-			#main,
-			#linkblocks,
-			#sb_container,
-			#searchbar,
-			#time,
-			#time-container,
-			#date,
-			#description,
-			#greetings,
-			#main #weather #tempContainer,
-			#main #weather #forecast,
-			#main #weather #current,
-			#quotes_container.shown,
-			#quotes_container.shown #quote,
-			#quotes_container.shown #author {
-				transition: none;
-			}
-		}
-
-		.he_hidden {
-			display: none !important;
-		}
-
-		// removes margin on tempContainer when both greetings and weather are hidden
-		#greetings.he_hidden + #weather #description.he_hidden + #tempContainer {
-			margin: 0;
-		}
-	}
 }
 
 @mixin allSettings {

src/styles/style.scss
@@ -1,6 +1,5 @@
 @import '../../node_modules/pocket-editor/dist/style';
 @import 'global';
-@include hideElements;
 
 #skiptosettings {
 	position: absolute;
@@ -66,16 +65,12 @@
 	padding: 12px;
 	box-sizing: border-box;
 	will-change: transform;
-
+	transition: opacity var(--load-time-transition), transform 0.4s;
 	text-shadow: 1px 2px 6px rgba(0, 0, 0, var(--text-shadow-alpha));
 	grid-template-areas: var(--grid);
 
 	&.pushed {
 		transform: translateX(-100px);
-	}
-
-	&.init {
-		opacity: 0;
 	}
 
 	&.move-edit {
@@ -690,15 +685,14 @@
 #showSettings {
 	position: fixed;
 	pointer-events: all;
-	opacity: 0;
 	transition: opacity 0.4s;
 }
 
 #creditContainer {
 	bottom: 0;
 	left: 0;
-	margin: 0 0 36px 24px;
 	opacity: 0;
+	padding: 0 0 36px 24px;
 	max-width: 50%;
 	display: flex;
 	align-items: center;
@@ -708,10 +702,8 @@
 		opacity: 1;
 		pointer-events: all;
 
-		&:hover {
-			#credit .exif {
-				opacity: 1;
-			}
+		&:hover #credit .exif {
+			opacity: 1;
 		}
 	}
 
@@ -739,9 +731,9 @@
 }
 
 #showSettings {
+	padding: 0 24px 24px 0;
 	bottom: 0;
 	right: 0;
-	padding: 0 24px 24px 0;
 	z-index: 3;
 
 	button {
@@ -763,20 +755,8 @@
 		}
 	}
 
-	&.init {
-		transition: opacity 0s;
-
-		button img {
-			transition: transform 0s;
-		}
-	}
-
-	// Doit etre dans cette ordre pour la hierarchie
-	&.enabled {
-		opacity: 1;
-	}
-
 	&.he_hidden {
+		display: flex;
 		opacity: 0;
 
 		&.shown {

commit c4f7036fb1d499348d8f9a7b3603af81113e488e

Fixed darkmode issue introduced with 0c4eba3

src/index.html
@@ -25,7 +25,7 @@
 		<style type="text/css" id="fontstyle"></style>
 	</head>
 
-	<body class="autodark init loading">
+	<body class="init loading">
 		<button id="skiptosettings" type="button">Skip to Settings</button>
 
 		<div id="editlink">

src/scripts/index.ts
@@ -1022,17 +1022,20 @@ export function darkmode(value: 'auto' | 'system' | 'enable' | 'disable', isEven
 
 	if (time) {
 		const cases = {
-			auto: time.now <= time.rise || time.now > time.set ? 'dark' : '',
+			auto: time.now <= time.rise || time.now > time.set ? 'dark' : 'light',
 			system: 'autodark',
 			enable: 'dark',
-			disable: '',
+			disable: 'light',
+		}
+
+		if (isEvent) {
+			clas(document.body, false, 'light')
+			clas(document.body, false, 'dark')
+			clas(document.body, false, 'autodark')
+			storage.sync.set({ dark: value })
 		}
 
 		clas(document.body, true, cases[value])
-
-		if (isEvent) {
-			storage.sync.set({ dark: value })
-		}
 	}
 }
 