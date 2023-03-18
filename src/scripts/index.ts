45f9b25538000fb09267b6e26685da8152c910d0
Adam Privette <aprivette@gmail.com>
2023-03-18 13:11:43 -0400

    Prevent rendering a zero at noon with 12 hour time

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -315,8 +315,13 @@ export function clock(
 			function numerical(date: Date) {
 				const fixunits = (val: number) => (val < 10 ? '0' : '') + val.toString()
 
-				let h = clock.ampm ? date.getHours() % 12 : date.getHours(),
-					m = fixunits(date.getMinutes()),
+				let h = date.getHours()
+
+				if (clock.ampm) {
+					h = h === 12 ? h : h % 12
+				}
+
+				let m = fixunits(date.getMinutes()),
 					s = fixunits(date.getSeconds())
 
 				const domclock = $('clock')
