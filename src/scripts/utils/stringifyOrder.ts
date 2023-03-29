63f9a8568fc5ed6dd33a1f873a99563c8e98a59f
Victor Azevedo <victor.azevedo@mail.com>
2023-03-29 11:55:42 +0200

    #191 Links order next to quicklinks properties

--- src/scripts/utils/stringifyOrder.ts
+++ src/scripts/utils/stringifyOrder.ts
@@ -1,13 +1,22 @@
-import { syncDefaults } from '../utils'
+import { Sync } from '../types/sync'
+import { syncDefaults, bundleLinks } from '../utils'
 
 // https://stackoverflow.com/a/53593328
 export default function stringifyOrder(data: { [key in string]: unknown }) {
+	const sync = { ...syncDefaults }
+	const orderedKeys = Object.keys(sync)
+
 	// Recursively get all keys
 	const keylist = new Set<string>()
 	JSON.stringify(data, (key, value) => (keylist.add(key), value))
 
-	// Use syncDefault order
-	const orderedKeys = Object.keys(syncDefaults)
+	// Add links after other links data
+	const links = bundleLinks(data as Sync)
+	if (links.length > 0) {
+		links.forEach((link) => orderedKeys.splice(orderedKeys.indexOf('linksrow') + 1, 0, link._id))
+	}
+
+	// Uses syncDefault order
 	const sortOrder = (a: string, b: string) => orderedKeys.indexOf(a) - orderedKeys.indexOf(b)
 
 	return JSON.stringify(data, Array.from(keylist).sort(sortOrder), 2)
