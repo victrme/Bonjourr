ff63742a225f8c848499137245b002d3de417f38
Victor Azevedo <victor.azevedo@mail.com>
2023-08-24 21:11:33 +0200

    Flattened Notes & use onSettingsLoad

--- src/scripts/features/notes.ts
+++ src/scripts/features/notes.ts
@@ -1,3 +1,4 @@
+import onSettingsLoad from '../utils/onsettingsload'
 import { eventDebounce } from '../utils/debounce'
 import { tradThis } from '../utils/translations'
 import pocketEditor from 'pocket-editor'
@@ -7,6 +8,9 @@ import langList from '../langs'
 import { Notes } from '../types/sync'
 
 type NotesEvent = { is: 'align' | 'width' | 'opacity' | 'change'; value: string }
+
+const container = document.getElementById('notes_container')
+let editor: any
 
 function translateNotesText() {
 	let lang = document.documentElement.getAttribute('lang')
@@ -23,72 +27,65 @@ function translateNotesText() {
 	return lines.join('')
 }
 
-export default async function notes(init: Notes | null, event?: NotesEvent) {
-	const container = document.getElementById('notes_container')
+function handleToggle(state: boolean) {
+	if (container) container?.classList.toggle('hidden', !state)
+}
 
-	function handleToggle(state: boolean) {
-		if (container) container?.classList.toggle('hidden', !state)
+function handleAlign(value: string) {
+	container?.classList.toggle('center-align', value === 'center')
+	container?.classList.toggle('right-align', value === 'right')
+}
+
+function handleWidth(value?: number) {
+	if (value) {
+		document.documentElement.style.setProperty('--notes-width', value.toString() + 'em')
 	}
+}
 
-	function handleAlign(value: string) {
-		container?.classList.toggle('center-align', value === 'center')
-		container?.classList.toggle('right-align', value === 'right')
+function handleOpacity(value: number) {
+	if (container) {
+		container?.classList.toggle('opaque', value > 0.45)
+		document.documentElement.style.setProperty('--notes-background-alpha', value.toString())
 	}
+}
 
-	function handleWidth(value?: number) {
-		if (value) {
-			document.documentElement.style.setProperty('--notes-width', value.toString() + 'em')
-		}
-	}
+async function updateNotes(event: NotesEvent) {
+	const notes = (await storage.sync.get('notes'))?.notes
 
-	function handleOpacity(value: number) {
-		if (container) {
-			container?.classList.toggle('opaque', value > 0.45)
-			document.documentElement.style.setProperty('--notes-background-alpha', value.toString())
-		}
-	}
-
-	function updateNotes(notes: Notes, event: NotesEvent) {
-		switch (event?.is) {
-			case 'change': {
-				notes.text = event.value
-				break
-			}
-
-			case 'align': {
-				handleAlign(event.value)
-				notes.align = event.value as typeof notes.align
-				break
-			}
-
-			case 'width': {
-				notes.width = parseInt(event.value)
-				handleWidth(notes.width)
-				break
-			}
-
-			case 'opacity': {
-				handleOpacity(parseFloat(event.value))
-				notes.opacity = parseFloat(event.value)
-				break
-			}
-		}
-
-		eventDebounce({ notes })
-	}
-
-	if (event) {
-		const data = await storage.sync.get('notes')
-		updateNotes(data.notes as Notes, event)
+	if (!notes) {
 		return
 	}
 
-	if (!init) return
-	if (document.getElementById('pocket-editor')) {
-		document.getElementById('pocket-editor')?.remove()
+	switch (event?.is) {
+		case 'change': {
+			notes.text = event.value
+			break
+		}
+
+		case 'align': {
+			handleAlign(event.value)
+			notes.align = event.value as typeof notes.align
+			break
+		}
+
+		case 'width': {
+			notes.width = parseInt(event.value)
+			handleWidth(notes.width)
+			break
+		}
+
+		case 'opacity': {
+			handleOpacity(parseFloat(event.value))
+			notes.opacity = parseFloat(event.value)
+			break
+		}
 	}
 
-	const editor = pocketEditor('notes_container')
+	eventDebounce({ notes })
+}
+
+function initNotes(init: Notes) {
+	editor = pocketEditor('notes_container')
 
 	handleAlign(init.align)
 	handleWidth(init.width)
@@ -96,5 +93,20 @@ export default async function notes(init: Notes | null, event?: NotesEvent) {
 	handleToggle(init.on)
 
 	editor.set(typeof init.text === 'string' ? init.text : translateNotesText())
-	editor.oninput(() => notes(init, { is: 'change', value: editor.get() }))
+
+	editor.oninput(() => {
+		updateNotes({ is: 'change', value: editor.get() })
+	})
+}
+
+export default function notes(init: Notes | null, event?: NotesEvent) {
+	if (event) {
+		updateNotes(event)
+		return
+	}
+
+	if (init) {
+		document.getElementById('pocket-editor')?.remove()
+		init.on ? initNotes(init) : onSettingsLoad(() => initNotes(init))
+	}
 }
