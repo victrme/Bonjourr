f532b0b01ac861e703a7750c89543077bf5966eb
Victor Azevedo <victor.azevedo@mail.com>
2023-11-18 10:42:17 +0100

    Fixed typo in hide breaking events

--- src/scripts/features/hide.ts
+++ src/scripts/features/hide.ts
@@ -1,18 +1,15 @@
 import storage from '../storage'
 import { Hide } from '../types/sync'
 
-export default async function hideElements(hidelist: Hide = {}, options?: { isEvent: true }) {
+export default async function hideElements(hide: Hide = {}, options?: { isEvent: true }) {
 	//
 	if (options?.isEvent) {
-		const { hide } = await storage.sync.get('hide')
-		const isOld = Array.isArray(hide) && hide.length === 4
-		if (isOld) return
-
-		hidelist = { ...hidelist, ...hide }
-		storage.sync.set({ hide: hidelist })
+		const sync = await storage.sync.get('hide')
+		hide = { ...sync.hide, ...hide }
+		storage.sync.set({ hide })
 	}
 
-	for (const [key, val] of Object.entries(hidelist)) {
+	for (const [key, val] of Object.entries(hide)) {
 		document.querySelector(`[data-hide="${key}"]`)?.classList.toggle('he_hidden', val)
 	}
 }
