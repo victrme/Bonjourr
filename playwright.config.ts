0c2399ab394a4cb303f7388ef40e11519bdf4710
Victor Azevedo <victor.azevedo@mail.com>
2023-12-10 17:03:41 +0100

    Working local dev server, requires python

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,43 +1,36 @@
 import { defineConfig, devices } from '@playwright/test'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
 	testDir: './src/tests',
+	outputDir: './src/tests/test-results',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
-	reporter: 'html',
+	reporter: 'null',
 	timeout: 3000,
 	use: {
-		baseURL: 'http://127.0.0.1:5500',
+		baseURL: 'http://127.0.0.1:5500/release/online/index.html',
 		trace: 'on-first-retry',
+	},
+	webServer: {
+		command: 'python -m http.server 5500',
+		url: 'http://localhost:5500/release/online/index.html',
+		reuseExistingServer: !process.env.CI,
 	},
 	projects: [
 		{
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] },
 		},
-
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] },
-		},
-
-		/* Test against mobile viewports. */
+		// {
+		// 	name: 'firefox',
+		// 	use: { ...devices['Desktop Firefox'] },
+		// },
+		// {
+		// 	name: 'webkit',
+		// 	use: { ...devices['Desktop Safari'] },
+		// },
 		// {
 		//   name: 'Mobile Chrome',
 		//   use: { ...devices['Pixel 5'] },
@@ -46,8 +39,6 @@ export default defineConfig({
 		//   name: 'Mobile Safari',
 		//   use: { ...devices['iPhone 12'] },
 		// },
-
-		/* Test against branded browsers. */
 		// {
 		//   name: 'Microsoft Edge',
 		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
@@ -57,11 +48,4 @@ export default defineConfig({
 		//   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
 		// },
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
 })
