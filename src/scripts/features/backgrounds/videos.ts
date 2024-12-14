f4862421398fda55cad6f34ece2f21c950eb4762
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2024-12-14 07:22:43 +0100

    Video as HTMLMedia

--- src/scripts/features/backgrounds/videos.ts
+++ src/scripts/features/backgrounds/videos.ts
@@ -3,14 +3,15 @@ interface VideosUpdate {
 }
 
 export default function videosBackgrounds(init: unknown, update?: VideosUpdate) {
-	const video = document.querySelector<HTMLVideoElement>('#video-background')
-	const overlay = document.querySelector<HTMLDivElement>('#image-background-overlay')
+	const video = document.querySelector<HTMLMediaElement>('#video-background')
+
+	video?.addEventListener('timeupdate', function (event) {
+		console.log(event)
+	})
 
 	if (init) {
-		if (video && overlay) {
+		if (video) {
 			video.src = 'https://cdn.pixabay.com/video/2022/06/13/120254-720504899_tiny.mp4'
-			overlay.style.opacity = '1'
-			console.log('hello world')
 		}
 	}
 }
