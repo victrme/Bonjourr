aeeec70bfc57cb7c3c9a08106e0d30bda7ca6e82
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2024-12-19 17:05:20 +0100

    Populating modal from JSON

--- src/scripts/utils/supporters.ts
+++ src/scripts/utils/supporters.ts
@@ -10,7 +10,7 @@ interface SupportersUpdate {
 
 const monthNames = [
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-];
+]
 
 export function supportersNotifications(init?: Sync.Supporters, update?: SupportersUpdate) {
     if (update) {
@@ -30,8 +30,8 @@ export function supportersNotifications(init?: Sync.Supporters, update?: Support
         const doc = document.importNode(template.content, true)
         const supporters_notif = doc.getElementById('supporters-notif')
 
-        // const currentMonth = 1 // january for testing
-        const currentMonth = new Date().getMonth() + 1 // production one
+        const currentMonth = 1 // january for testing
+        // const currentMonth = new Date().getMonth() + 1 // production one
 
         // if it's a new month and notif was closed previously
         if (!supporters_notif || currentMonth === storedMonth && wasClosed) {
@@ -71,6 +71,7 @@ export function supportersNotifications(init?: Sync.Supporters, update?: Support
             supporters_notif.addEventListener("click", function (event) {
                 if (event.target instanceof Element && !event.target.closest('#supporters-notif-close')) {
                     supportersModal(undefined, true)
+                    populateModal()
                 }
             })
         })
@@ -188,5 +189,34 @@ export function supportersModal(init?: boolean, state?: boolean) {
             document.documentElement.removeAttribute('supporters_modal_open')
         }
     }
+}
 
+let modalPopulated = false
+export async function populateModal() {
+    if (modalPopulated) return
+
+    // let response: Response | undefined
+    // response = await fetch("https://bonjourr-kofi.pages.dev/list")
+    // console.log(response)
+
+    const json = [{ "date": "03/10/2024 22:49:00", "name": "Randy", "amount": 2, "monthly": true, "paidWith": "stripe", "hashedEmail": "LTE2NDA4MDM3MDg=" }, { "date": "14/10/2024 04:27:00", "name": "Ko-fi Supporter", "amount": 5, "monthly": false, "paidWith": "stripe", "hashedEmail": "LTIwNDQwODU3MTk=" }, { "date": "17/10/2024 15:41:00", "name": "Mark B", "amount": 10, "monthly": false, "paidWith": "stripe", "hashedEmail": "LTExNjI5MTg1MDY=" }, { "date": "19/10/2024 18:37:00", "name": "Levon", "amount": 5, "monthly": false, "paidWith": "stripe", "hashedEmail": "LTE4ODE0NTY2Njk=" }, { "date": "28/10/2024 09:56:00", "name": "Francis", "amount": 5, "monthly": true, "paidWith": "stripe", "hashedEmail": "MTQ0MTA3MjM2OA==" }, { "date": "03/11/2024 09:37:00", "name": "Marcel", "amount": 5, "monthly": false, "paidWith": "stripe", "hashedEmail": "MTc1NjE0MDc1" }, { "date": "03/11/2024 22:49:00", "name": "Randy", "amount": 2, "monthly": true, "paidWith": "stripe", "hashedEmail": "LTE2NDA4MDM3MDg=" }, { "date": "07/11/2024 16:47:00", "name": "Ros", "amount": 5, "monthly": false, "paidWith": "stripe", "hashedEmail": "NDEzMDUyOTI5" }, { "date": "28/11/2024 09:56:00", "name": "Francis", "amount": 5, "monthly": true, "paidWith": "stripe", "hashedEmail": "MTQ0MTA3MjM2OA==" }, { "date": "30/11/2024 00:22:00", "name": "SteveWhite", "amount": 5, "monthly": false, "paidWith": "stripe", "hashedEmail": "MTk1NjIwMTY2OA==" }]
+
+    // sorts in descending order
+    json.sort((a, b) => b.amount - a.amount)
+
+    const monthlyFragment = document.createDocumentFragment()
+    const onceFragment = document.createDocumentFragment()
+
+    json.forEach((supporter: { name: string, monthly: boolean }) => {
+        const li = document.createElement('li')
+        li.innerHTML = supporter.name
+        
+        const targetFragment = supporter.monthly ? monthlyFragment : onceFragment
+        targetFragment.appendChild(li)
+    })
+
+    document.querySelector('#supporters-modal #monthly')?.appendChild(monthlyFragment)
+    document.querySelector('#supporters-modal #once')?.appendChild(onceFragment)
+
+    modalPopulated = true
 }
