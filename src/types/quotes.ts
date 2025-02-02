a1cdbf7f30961ddaf0036c07d39d615449087c28
Stelage <stelage@protonmail.com>
2025-02-02 22:44:57 +0100

    rename 'the-office' key to 'office'

--- src/types/quotes.ts
+++ src/types/quotes.ts
@@ -4,7 +4,7 @@ declare namespace Quotes {
 		content: string
 	}
 
-	type Types = 'classic' | 'kaamelott' | 'inspirobot' | 'stoic' | 'hitokoto' | 'the-office' | 'user' | 'url'
+	type Types = 'classic' | 'kaamelott' | 'inspirobot' | 'stoic' | 'hitokoto' | 'office' | 'user' | 'url'
 
 	type Sync = {
 		on: boolean
