3563f922921b09c35d29c7a0d81fb9fdbef67c22
Victor Azevedo <victor.azevedo@mail.com>
2025-03-21 08:32:32 +0100

    Separate user actions to events.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -20,6 +20,7 @@ import { needsChange, userDate, suntime } from './shared/time'
 import { onSettingsLoad } from './utils/onsettingsload'
 import { filterImports } from './imports'
 import { settingsInit } from './settings'
+import { userActions } from './events'
 import { storage } from './storage'
 import 'clickdown'
 
@@ -94,8 +95,8 @@ async function startup() {
 	onInterfaceDisplay(() => {
 		document.body.classList.remove('init')
 
-		userActionsEvents()
 		setPotatoComputerMode()
+		userActions()
 
 		supportersNotifications({
 			supporters: sync.supporters,
@@ -103,10 +104,10 @@ async function startup() {
 		})
 
 		interfacePopup({
-			old: oldVersion,
-			new: CURRENT_VERSION,
-			review: sync.review ?? 0,
 			announce: sync.announcements,
+			review: sync.review ?? 0,
+			new: CURRENT_VERSION,
+			old: oldVersion,
 		})
 	})
 }
@@ -171,143 +172,6 @@ function onInterfaceDisplay(callback?: () => undefined): void {
 		interfaceDisplayCallback = callback
 	} else {
 		interfaceDisplayCallback()
-	}
-}
-
-function userActionsEvents() {
-	const domsuggestions = document.getElementById('sb-suggestions')
-	let isMousingDownOnInput = false
-
-	document.body.addEventListener('mousedown', detectTargetAsInputs)
-	document.getElementById('b_editmove')?.addEventListener('click', closeSettingsOnMoveOpen)
-
-	document.addEventListener('click', clickUserActions)
-	document.addEventListener('keydown', keydownUserActions)
-	document.addEventListener('keyup', keydownUserActions)
-
-	function keydownUserActions(event: KeyboardEvent) {
-		if (event.code === 'Escape') {
-			if (domsuggestions?.classList.contains('shown')) {
-				domsuggestions?.classList.remove('shown')
-				return
-			}
-
-			const open = isOpen()
-			const keyup = event.type === 'keyup'
-
-			if (open.contextmenu) {
-				document.dispatchEvent(new Event('close-edit'))
-			}
-			//
-			else if (open.settings && keyup) {
-				document.dispatchEvent(new Event('toggle-settings'))
-			}
-			//
-			else if (open.selectall) {
-				document.dispatchEvent(new Event('remove-select-all'))
-			}
-			//
-			else if (open.folder) {
-				document.dispatchEvent(new Event('close-folder'))
-			}
-			//
-			else if (keyup) {
-				// condition to avoid conflicts with esc key on supporters modal
-				// likely to be improved
-				if (document.documentElement.dataset.supportersModal === undefined) {
-					document.dispatchEvent(new Event('toggle-settings'))
-				}
-			}
-
-			return
-		}
-
-		if (event.code === 'Tab') {
-			document.body.classList.toggle('tabbing', true)
-			return
-		}
-	}
-
-	function clickUserActions(event: MouseEvent) {
-		if (isMousingDownOnInput) {
-			return
-		}
-
-		const open = isOpen()
-		const composedPath = (event.composedPath() as Element[]) ?? [document.body]
-		const path = composedPath.filter(node => node?.className?.includes)
-		const pathIds = path.map(el => (el as HTMLElement).id)
-
-		const on = {
-			body: (path[0] as HTMLElement).tagName === 'BODY',
-			link: path.some(el => el.classList.contains('link')),
-			linkfolder: path.some(el => el.className.includes('folder')),
-			addgroup: path.some(el => el.className.includes('add-group')),
-			folder: path.some(el => el.className.includes('in-folder')),
-			interface: pathIds.includes('interface'),
-			editlink: pathIds.includes('editlink'),
-			settings: path.some(el => el.id === 'settings'),
-			showsettings: path.some(el => el.id === 'show-settings'),
-		}
-
-		if (document.body.classList.contains('tabbing')) {
-			document.body?.classList.toggle('tabbing', false)
-		}
-
-		if (on.showsettings) {
-			document.dispatchEvent(new Event('toggle-settings'))
-		}
-
-		if (open.contextmenu && !on.editlink) {
-			if (on.addgroup && document.querySelector('.link-title.add-group.selected')) {
-				return
-			}
-
-			document.dispatchEvent(new Event('close-edit'))
-			return
-		}
-
-		if ((on.body || on.interface) === false) {
-			return
-		}
-
-		if (open.settings) {
-			document.dispatchEvent(new Event('toggle-settings'))
-		}
-		//
-		else if (open.selectall && !on.link) {
-			document.dispatchEvent(new Event('remove-select-all'))
-		}
-		//
-		else if (open.folder && !on.folder && !on.linkfolder) {
-			document.dispatchEvent(new Event('close-folder'))
-		}
-	}
-
-	function isOpen() {
-		return {
-			settings: !!document.getElementById('settings')?.classList.contains('shown'),
-			folder: !!document.querySelector('.in-folder'),
-			selectall: document.getElementById('linkblocks')?.classList.contains('select-all'),
-			contextmenu: document.querySelector<HTMLDialogElement>('#editlink')?.open,
-		}
-	}
-
-	function detectTargetAsInputs(event: Event) {
-		const path = event.composedPath() as Element[]
-		const tagName = path[0]?.tagName ?? ''
-		isMousingDownOnInput = ['TEXTAREA', 'INPUT'].includes(tagName)
-	}
-
-	function closeSettingsOnMoveOpen() {
-		setTimeout(() => {
-			const elementmover = document.getElementById('element-mover')
-			const moverHasOpened = elementmover?.classList.contains('hidden') === false
-
-			if (moverHasOpened) {
-				document.dispatchEvent(new Event('toggle-settings'))
-			}
-		}, 20)
 	}
 }
 
