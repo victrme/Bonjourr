c222637b340863c97557d7bea7a8fa062aaac30a
Victor Azevedo <victor.azevedo@mail.com>
2025-04-26 08:52:56 +0200

    Add more old configs, working tests

--- tests/imports.test.ts
+++ tests/imports.test.ts
@@ -1,6 +1,8 @@
 import { assert } from '@std/assert'
 import './init.test.ts'
 
+import type { Link } from '../src/types/shared.ts'
+
 // Import script after test init, document needs to be loaded first
 import { SYNC_DEFAULT } from '../src/scripts/defaults.ts'
 import { filterImports } from '../src/scripts/imports.ts'
@@ -23,14 +25,42 @@ Deno.test('Current version small import', () => {
 	assert(defaults.lang === config.lang)
 })
 
-Deno.test('1.10', () => {
-	const text = Deno.readTextFileSync('./tests/configs/1.10.0.json')
+Deno.test('1.10', async (t) => {
+	const text = Deno.readTextFileSync('./tests/configs/old.1.10.0.json')
 	const old = JSON.parse(text)
 	const res = filterImports(defaults, old)
 
-	assert(Array.isArray(res.hide) === false)
-	assert(old.background_blur === res.backgrounds.blur)
-	assert(old.searchbar_engine === res.searchbar.engine)
-	assert(JSON.stringify(res).includes('https://www.youtube.com/'))
-	assert(JSON.stringify(res).includes('https://api.faviconkit.com/www.youtube.com/144'))
+	await t.step('Hide', () => {
+		assert(Array.isArray(res.hide) === false)
+	})
+
+	await t.step('Links', () => {
+		const allkeys = Object.keys(res)
+		const linkkeys = allkeys.filter((key) => key.length === 11 && key.includes('links'))
+
+		// Correct fields
+		assert(res.links === undefined)
+		assert(linkkeys.length > 0)
+
+		// Correct links
+		assert(JSON.stringify(res).includes('https://www.youtube.com/'))
+		assert(JSON.stringify(res).includes('https://api.faviconkit.com/www.youtube.com/144'))
+
+		// Links are valid
+		const key = linkkeys[0]
+		const link = res[key] as Link
+
+		assert(typeof link._id === 'string')
+		assert(typeof link.order === 'number')
+		assert(link.parent === 'default')
+	})
+
+	await t.step('Backgrounds', () => {
+		assert(old.background_blur === res.backgrounds.blur)
+	})
+
+	await t.step('Searchbar', () => {
+		assert(old.searchbar_engine === res.searchbar.engine)
+		assert(old.searchbar_newtab === res.searchbar.newtab)
+	})
 })
