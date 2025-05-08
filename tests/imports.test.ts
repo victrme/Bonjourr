40d800b4361c493c38c00e06ca4ffc69c752a252
Victor Azevedo <victor.azevedo@mail.com>
2025-05-08 22:00:06 +0200

    Improve latest version tests

--- tests/imports.test.ts
+++ tests/imports.test.ts
@@ -3,7 +3,7 @@ import './init.test.ts'
 // Import script after test init, document needs to be loaded first
 import { SYNC_DEFAULT } from '../src/scripts/defaults.ts'
 import { filterImports } from '../src/scripts/imports.ts'
-import type { Link, OldSync } from '../src/types/shared.ts'
+import type { Link } from '../src/types/shared.ts'
 import { assert } from '@std/assert'
 
 const defaults = structuredClone(SYNC_DEFAULT)
@@ -33,31 +33,31 @@ Deno.test('1.10.0', async (t) => {
 	const old = JSON.parse(text)
 	const res = filterImports(defaults, old)
 
-	await t.step('Links', () => {
+	await t.step('Links', async (t) => {
 		const allkeys = Object.keys(res)
 		const linkkeys = allkeys.filter(
 			(key) => key.length === 11 && key.includes('links'),
 		)
 
-		// Correct fields
-		assert(res.links === undefined)
-		assert(linkkeys.length > 0)
+		await t.step('Correct fields', () => {
+			assert(res.links === undefined)
+			assert(linkkeys.length > 0)
+		})
 
-		// Correct links
-		assert(JSON.stringify(res).includes('https://www.youtube.com/'))
-		assert(
-			JSON.stringify(res).includes(
-				'https://api.faviconkit.com/www.youtube.com/144',
-			),
-		)
+		await t.step('Correct values', () => {
+			const str = JSON.stringify(res)
+			assert(str.includes('https://www.youtube.com/'))
+			assert(str.includes('https://api.faviconkit.com/www.youtube.com/144'))
+		})
 
-		// Links are valid
-		const key = linkkeys[0]
-		const link = res[key] as Link
+		await t.step('Valid links', () => {
+			const key = linkkeys[0]
+			const link = res[key] as Link
 
-		assert(typeof link._id === 'string')
-		assert(typeof link.order === 'number')
-		assert(link.parent === 'default')
+			assert(typeof link._id === 'string')
+			assert(typeof link.order === 'number')
+			assert(link.parent === 'default')
+		})
 	})
 
 	await t.step('Clock', () => {
@@ -68,9 +68,14 @@ Deno.test('1.10.0', async (t) => {
 		assert(old.background_blur === res.backgrounds.blur)
 	})
 
-	await t.step('Searchbar', () => {
-		assert(old.searchbar_engine === res.searchbar.engine)
-		assert(old.searchbar_newtab === res.searchbar.newtab)
+	await t.step('Searchbar', async (t) => {
+		await t.step('Engine', () => {
+			assert(old.searchbar_engine === res.searchbar.engine)
+		})
+
+		await t.step('New tab', () => {
+			assert(old.searchbar_newtab === res.searchbar.newtab)
+		})
 	})
 
 	await t.step('Hide', () => {
@@ -87,10 +92,25 @@ Deno.test('20.4.2', async (t) => {
 	const old = JSON.parse(text)
 	const res = filterImports(defaults, old)
 
-	await t.step('Backgrounds', () => {
-		assert(old.unsplash.every === res.backgrounds.frequency)
-		assert(old.background_blur === res.backgrounds.blur)
-		assert(old.background_bright === res.backgrounds.bright)
-		assert(old.unsplash.collection === res.backgrounds.queries['unsplash-images-collections'])
+	await t.step('Backgrounds', async (t) => {
+		await t.step('Local type', () => {
+			assert(res.backgrounds.type === 'files')
+		})
+
+		await t.step('Blur', () => {
+			assert(old.background_blur === res.backgrounds.blur)
+		})
+
+		await t.step('Bright', () => {
+			assert(old.background_bright === res.backgrounds.bright)
+		})
+
+		await t.step('Unsplash collection', () => {
+			assert(old.unsplash.collection === res.backgrounds.queries['unsplash-images-collections'])
+		})
+
+		await t.step('Frequency', () => {
+			assert(old.unsplash.every === res.backgrounds.frequency)
+		})
 	})
 })
