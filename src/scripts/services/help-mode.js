ae959ad329553fc313032b111b12ea39fee2c15f
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2025-08-30 21:34:37 +0200

    [help mode] working export button

--- src/scripts/services/help-mode.js
+++ src/scripts/services/help-mode.js
@@ -1,9 +1,11 @@
+let hasExportedSettings = false
+
 globalThis.window.addEventListener('load', function () {
-	// document.body.addEventListener('keydown', toggleHelpMode)
+	document.body.addEventListener('keydown', toggleHelpMode)
 
 	globalThis.setTimeout(() => {
 		displayHelpModePrompt()
-	}, 5000)
+	}, 50)
 })
 
 function displayHelpModePrompt() {
@@ -18,20 +20,63 @@ function displayHelpModePrompt() {
 
 	let helpModeBtn = document.getElementById('open-help-mode')
 	helpModeBtn.addEventListener('click', toggleHelpMode)
+
+	this.document.querySelector('.export').addEventListener("click", downloadSettings)
+}
+
+function downloadSettings() {
+	if (chrome?.storage) {
+		chrome.storage.sync.get(null, (items) => {
+			const json = JSON.stringify(items, null, 2);
+			const blob = new Blob([json], { type: "application/json" });
+			const url = URL.createObjectURL(blob);
+
+			const a = document.createElement("a");
+			a.href = url;
+			a.download = "bonjourr-settings.json";
+			a.click();
+
+			URL.revokeObjectURL(url); // clean up
+		})
+
+		if (document.querySelector('.reset')) {
+			document.querySelector('.reset').disabled = false
+		}
+
+		hasExportedSettings = true
+	} else {
+
+	}
+}
+
+function resetOnce() {
+	let resetBtn = document.querySelector('.reset')
+	resetBtn.title = "You're about to reset Bonjourr to its default configuration."
+	resetBtn.querySelector('span').textContent = "Are you sure?"
+	resetBtn.classList.add('danger')
+	resetBtn.addEventListener("click", resetApply)
+}
+
+function resetApply() {
+	alert('goodbye')
 }
 
 /**
- * @param {KeyboardEvent} event
+ * @param {KeyboardEvent | MouseEvent} event
  * @returns {void}
  */
 function toggleHelpMode(event) {
-	// const { key, shiftKey, ctrlKey } = event
-	// const questionMarkKey = key === ',' || key === '/' || key === '?'
-	// const ctrlShiftQuestion = ctrlKey && shiftKey && questionMarkKey
+	if (event.type === "keydown") {
+		const { key, shiftKey, ctrlKey } = event
+		const questionMarkKey = key === ',' || key === '/' || key === '?'
+		const ctrlShiftQuestion = ctrlKey && shiftKey && questionMarkKey
 
-	// if (!ctrlShiftQuestion) {
-	// 	return
-	// }
+		if (!ctrlShiftQuestion) {
+			return
+		}
+	} else if (event.type !== 'click') {
+		return
+	} 
 
 	if (!document.getElementById('help-mode')) {
 		createHelpModeDisplay()
@@ -54,7 +99,16 @@ function createHelpModeDisplay() {
 	const fragment = template.content.cloneNode(true)
 	const container = fragment.querySelector('#help-mode')
 	const startTimer = globalThis.performance.now()
+	
 	document.documentElement.prepend(container)
+	
+	const resetBtn = this.document.querySelector('.reset')
+	this.document.querySelector('.export').addEventListener("click", downloadSettings)
+	resetBtn.addEventListener("click", resetOnce)
+	
+	if (hasExportedSettings) {
+		resetBtn.disabled = false
+	}
 
 	function setStatus(statusID, resp) {
 		const endTimer = Math.round(globalThis.performance.now() - startTimer)
