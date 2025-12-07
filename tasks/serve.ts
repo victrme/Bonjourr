e683ca1c744a6b5f037a4d049af8b1fbadf44b10
victrme <victor.azevedo@mail.com>
2025-12-07 09:31:11 +0100

    Create separate serve task

---
+++ tasks/serve.ts
@@ -0,0 +1,71 @@
+import { extname } from '@std/path'
+
+const port = Deno.args[0]
+
+if (port) {
+	httpServer(parseInt(port))
+}
+
+export function httpServer(port = 8000, baseUrl = 'release/online') {
+	const contentTypeList: Record<string, string> = {
+		'.webmanifest': 'application/manifest+json',
+		'.js': 'text/javascript',
+		'.svg': 'image/svg+xml',
+		'.ico': 'image/x-icon',
+		'.mp3': 'audio/mpeg',
+		'.html': 'text/html',
+		'.png': 'image/png',
+		'.css': 'text/css',
+	}
+
+	// Request handler
+	// actual meat of the server
+
+	const serverHandler: Deno.ServeHandler<Deno.NetAddr> = async (request) => {
+		// Find resource
+
+		const url = new URL(request.url)
+		const filePath = baseUrl + (url.pathname === '/' ? '/index.html' : url.pathname)
+		const fileExists = await Deno.stat(filePath)
+
+		if (!fileExists) {
+			return new Response('Not Found', {
+				status: 404,
+			})
+		}
+
+		// Find content type
+
+		const headers: HeadersInit = { 'cache-control': 'no-cache' }
+		const fileExt = extname(filePath)
+		const contentType = contentTypeList[fileExt]
+
+		if (contentType) {
+			headers.contentType = contentType
+		}
+
+		// Read data
+
+		const data = await Deno.readFile(filePath)
+
+		return new Response(data, {
+			headers: headers,
+			status: 200,
+		})
+	}
+
+	// Starts server
+	// catches errors
+
+	Deno.serve({ port }, async (request, info) => {
+		try {
+			return await serverHandler(request, info)
+		} catch (err) {
+			if (err instanceof Deno.errors.NotFound) {
+				return new Response('Not Found', { status: 404 })
+			} else {
+				return new Response('Internal Server Error', { status: 500 })
+			}
+		}
+	})
+}
