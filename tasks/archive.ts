eeb91664a6bc93b8bca111bb292613dc603657ea
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2026-05-30 19:10:59 +0200

    Simplified scripts, fix docs

--- tasks/archive.ts
+++ tasks/archive.ts
@@ -2,26 +2,22 @@ const VERSION = '22.0.1'
 
 buildPlatforms()
 archiveSource()
-
-for await (const entry of Deno.readDir('./release')) {
-    if (entry.isDirectory) {
-        archivePlatform(entry.name)
-    }
-}
+archivePlatform('chrome')
+archivePlatform('edge')
+archivePlatform('firefox')
+archivePlatform('online')
 
 /*
  * Functions
  */
 
 function buildPlatforms(): void {
-    console.log('Build platforms')
-
     new Deno.Command('deno', { args: ['task', 'build'] }).outputSync()
 }
 
 function archiveSource(): void {
     const files = ['_locales', 'docker', 'src', 'tasks', 'deno.json', 'deno.lock', 'README.md']
-    const outfile = `./release/firefox/bonjourr-source-${VERSION}.zip`
+    const outfile = `./release/bonjourr-source-${VERSION}.zip`
     const args = ['-r', outfile, ...files]
 
     console.log('Archiving source')
@@ -30,7 +26,7 @@ function archiveSource(): void {
 }
 
 function archivePlatform(name: string): void {
-    const args = ['-r', `bonjourr-${name}-${VERSION}.zip`, '.']
+    const args = ['-r', `../bonjourr-${name}-${VERSION}.zip`, '.']
     const cwd = `./release/${name}`
 
     console.log(`Archiving ${name}`)
